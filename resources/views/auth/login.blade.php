d6535fc7e9e3323df69265b7d61a33d78ecba647
Caniscent <rangga.pramudya23@gmail.com>
2024-09-27 21:03:11 +0700

    mengubah metode login dari email ke name

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,32 +3,36 @@
 @section('content')
 <div class="flex items-center justify-center min-h-screen bg-gray-50">
     <div class="w-full max-w-md p-8 space-y-8 shadow-lg card bg-white">
-        <h2 class="text-2xl font-bold text-center text-gray-800">{{ __('Login') }}</h2>
+        <h2 class="text-2xl font-bold text-center text-gray-800">
+            <a href="{{url('/')}}" class="text-2xl font-bold text-center text-blue-400 hover:underline">{{ __('Sano Care') }}</a>
+            {{ __('Login') }}
+        </h2>
 
         <form method="POST" action="{{ route('login') }}" class="space-y-6">
             @csrf
 
-            <!-- Email Input -->
+            <!-- Name Input -->
             <div class="form-control">
-                <label for="email" class="label">
-                    <span class="label-text">{{ __('Email Address') }}</span>
+                <label for="name" class="label">
+                    <span class="label-text text-black">{{ __('Name') }}</span>
                 </label>
-                <input id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus
-                       class="input input-bordered w-full @error('email') input-error @enderror">
-                @error('email')
+                <input id="name" type="text" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus
+                       class="input input-bordered w-full @error('name') input-error @enderror bg-blue-200 text-black">
+                @error('name')
                     <span class="text-error">
                         <strong>{{ $message }}</strong>
                     </span>
                 @enderror
             </div>
 
+
             <!-- Password Input -->
             <div class="form-control">
                 <label for="password" class="label">
-                    <span class="label-text">{{ __('Password') }}</span>
+                    <span class="label-text text-black">{{ __('Password') }}</span>
                 </label>
                 <input id="password" type="password" name="password" required autocomplete="current-password"
-                       class="input input-bordered w-full @error('password') input-error @enderror">
+                       class="input input-bordered w-full @error('password') input-error @enderror bg-blue-200 text-black">
                 @error('password')
                     <span class="text-error">
                         <strong>{{ $message }}</strong>
@@ -40,7 +44,7 @@
             <div class="form-control">
                 <label class="cursor-pointer label">
                     <input type="checkbox" name="remember" class="checkbox" {{ old('remember') ? 'checked' : '' }}>
-                    <span class="label-text">{{ __('Remember Me') }}</span>
+                    <span class="label-text text-black">{{ __('Remember Me') }}</span>
                 </label>
             </div>
 
@@ -50,18 +54,20 @@
             </div>
 
             <!-- Forgot Password -->
-            @if (Route::has('password.request'))
+            {{-- @if (Route::has('password.request'))
                 <div class="text-center">
                     <a class="link link-primary" href="{{ route('password.request') }}">
                         {{ __('Forgot Your Password?') }}
                     </a>
                 </div>
-            @endif
+            @endif --}}
 
+            {{-- Register --}}
             @if (Route::has('register'))
-                <div class="text-center">
-                    <a class="link link-primary" href="{{ route('register') }}">
-                        {{ __("Don't have account? register now") }}
+                <div class="text-center text-black">
+                    Don't have account?
+                    <a class="text-blue-700 hover:underline" href="{{ route('register') }}">
+                        {{ __(" register now") }}
                     </a>
                 </div>
             @endif
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,10 @@
 <div class="flex justify-center items-center min-h-screen">
     <div class="card w-full max-w-md shadow-lg bg-gray-50">
         <div class="card-body">
-            <h2 class="card-title text-center text-2xl text-black">{{ __('Register') }}</h2>
+            <h2 class="text-2xl font-bold text-center text-gray-800">
+                <a href="{{url('/')}}" class="text-2xl font-bold text-center text-blue-400 hover:underline">{{ __('Sano Care') }}</a>
+                {{ __('Register') }}
+            </h2>
 
             <form method="POST" action="{{ route('register') }}">
                 @csrf
@@ -13,7 +16,7 @@
                     <label for="name" class="label">
                         <span class="label-text text-black">{{ __('Name') }}</span>
                     </label>
-                    <input id="name" type="text" class="input input-bordered @error('name') input-error @enderror bg-blue-200" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                    <input id="name" type="text" class="input input-bordered @error('name') input-error @enderror bg-blue-200 text-black" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                     @error('name')
                         <span class="text-error text-sm">
@@ -26,7 +29,7 @@
                     <label for="email" class="label">
                         <span class="label-text text-black">{{ __('Email Address') }}</span>
                     </label>
-                    <input id="email" type="email" class="input input-bordered @error('email') input-error @enderror bg-blue-200" name="email" value="{{ old('email') }}" required autocomplete="email">
+                    <input id="email" type="email" class="input input-bordered @error('email') input-error @enderror bg-blue-200 text-black" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                     @error('email')
                         <span class="text-error text-sm">
@@ -39,7 +42,7 @@
                     <label for="password" class="label">
                         <span class="label-text text-black">{{ __('Password') }}</span>
                     </label>
-                    <input id="password" type="password" class="input input-bordered @error('password') input-error @enderror bg-blue-200" name="password" required autocomplete="new-password">
+                    <input id="password" type="password" class="input input-bordered @error('password') input-error @enderror bg-blue-200 text-black" name="password" required autocomplete="new-password">
 
                     @error('password')
                         <span class="text-error text-sm">
@@ -52,14 +55,23 @@
                     <label for="password-confirm" class="label">
                         <span class="label-text text-black">{{ __('Confirm Password') }}</span>
                     </label>
-                    <input id="password-confirm" type="password" class="input input-bordered bg-blue-200" name="password_confirmation" required autocomplete="new-password">
+                    <input id="password-confirm" type="password" class="input input-bordered bg-blue-200 text-black" name="password_confirmation" required autocomplete="new-password">
                 </div>
 
-                <div class="form-control mt-6">
+                <div class="form-control mt-10">
                     <button type="submit" class="btn btn-primary w-full">
                         {{ __('Register') }}
                     </button>
                 </div>
+
+                {{-- Login --}}
+                @if (Route::has('login'))
+                    <div class="text-center mt-3">
+                        <a class="text-blue-700 hover:underline" href="{{ route('login') }}">
+                            {{ __('Back to login') }}
+                        </a>
+                    </div>
+                @endif
             </form>
         </div>
     </div>
