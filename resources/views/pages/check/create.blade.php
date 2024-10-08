1bcc56a8a14463fd1682bd7ae0a12704874d8285
Caniscent <rangga.pramudya23@gmail.com>
2024-10-08 09:01:23 +0700

    memperbarui fe create pada checks

--- resources/views/pages/check/create.blade.php
+++ resources/views/pages/check/create.blade.php
@@ -53,12 +53,23 @@
 
                     {{-- sugar level input --}}
                     <div class="mb-5">
-                        <label for="sugar_level" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Kadar Gula</label>
+                        <label for="sugar" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Kadar Gula</label>
                         <div class="relative">
-                            <input type="number" name="sugar_level" id="sugar_level" class="bg-gray-50 no-spinners border @error('sugar_level') border-red-500 @else border-gray-300 @enderror text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-20 dark:bg-blue-200 dark:border-blue-100 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <input type="number" name="sugar" id="sugar" class="bg-gray-50 no-spinners border @error('sugar') border-red-500 @else border-gray-300 @enderror text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-20 dark:bg-blue-200 dark:border-blue-100 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                             <span class="absolute right-3 top-2 text-sm text-dark dark:text-gray-900">mg/dL</span>
                         </div>
-                        @error('sugar_level')
+                        @error('sugar')
+                            <p class="mt-1 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    {{-- uji input --}}
+                    <div class="mb-5">
+                        <label for="test_method" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Method Uji</label>
+                        <div class="relative">
+                            <input type="number" name="test_method" id="test_method" class="bg-gray-50 no-spinners border @error('test_method') border-red-500 @else border-gray-300 @enderror text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-20 dark:bg-blue-200 dark:border-blue-100 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                        </div>
+                        @error('test_method')
                             <p class="mt-1 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
                         @enderror
                     </div>
