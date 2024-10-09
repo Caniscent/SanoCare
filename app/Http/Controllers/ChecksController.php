20f814a3444ce4aa82f05baca1c0226a5e7abe16
Adrella Arte <oujireflect.me@gmail.com>
2024-10-09 16:37:28 +0700

    PersonalNeed

--- app/Http/Controllers/ChecksController.php
+++ app/Http/Controllers/ChecksController.php
@@ -20,34 +20,46 @@ class ChecksController extends Controller
      */
     public function index()
     {
-        $data = ChecksModel::where('user_id', auth::id())->with('user')->get();
 
+    }
+
+    public function calculatePersonalNeed($weight, $height, $age, $gender ,$bloodSugar, $testMethod, $activity) {
         $prediabetes = false;
 
-        foreach ($data as $item) {
-            if ($item->test_method == 'puasa') {
-                if ($item->sugar >= 100 && $item->sugar <= 125) {
-                    $prediabetes = true;
-                }
-                else if ($item->sugar > 125) {
-                    //Diabetes
-                }
+        if ($testMethod == 'puasa') {
+            if ($bloodSugar >= 100 && $bloodSugar <= 125) {
+                $prediabetes = true;
             }
-            else if ($item->test_method == 'ttgo') {
-                if ($item->sugar >= 140 && $item->sugar <= 199) {
-                    $prediabetes = true;
-                }
-                else if ($item->sugar > 199) {
-                    //Diabetes
-                }
+            else if ($bloodSugar > 125) {
+                return [
+                    "error" => True,
+                    "code" => "DIABETES",
+                ];
             }
-            else {
-                // INVALID BLODD SUGAR TEST
+        }
+        else if ($testMethod == 'ttgo') {
+            if ($bloodSugar >= 140 && $bloodSugar <= 199) {
+                $prediabetes = true;
             }
+            else if ($bloodSugar > 199) {
+                return [
+                    "error" => True,
+                    "code" => "DIABETES",
+                ];
+            }
+        }
+        else {
+            return [
+                "error" => True,
+                "code" => "INVALID_BLOOD_SUGAR_TEST",
+            ];
         }
 
         if (!$prediabetes) {
-            // TIDAK PRE DIABETES
+            return [
+                "error" => True,
+                "code" => "NOT_PRE_DIABETES",
+            ];
         }
 
         // Calculate BMI
@@ -58,15 +70,49 @@ class ChecksController extends Controller
             "obese" => 30,
         ];
 
-        foreach ($data as $item) {
-            $bmi = round($item->weight / (($item->height / 100) ** 2), 2);
-        }
+        // Perhitungan BMI
+        $bmi = round($weight / (($height / 100) ** 2), 2);
 
         foreach ($body_level as $category => $threshold ) {
-            //
+            if ($bmi < $threshold) {
+                $body = [$bmi,$category];
+            }
+            break;
         }
 
-        // return view('pages.check.index', ['data' => $data]);
+        if (!$body) {
+            $body = [$bmi,'obese'];
+        }
+
+        // Calculate Daily Categories
+        $cons = ($gender == 'perempuan') ? -161 : 5;
+        $daily_calories = (10 * $weight) + (6.25 * $height) - (5 * $age) + $cons;
+
+        $activity_level = [
+            "very light" => 1.2,
+            "light" => 1.375,
+            "medium" => 1.55,
+            "heavy" => 1.725,
+        ];
+
+        $daily_calories += $activity_level[$activity];
+
+        $required_carb = round($daily_calories * 0.5  / 4, 2);
+        $required_prot = round($daily_calories * 0.2  / 4, 2);
+        $required_fat  = round($daily_calories * 0.2  / 9, 2);
+        $required_fibr = round($daily_calories * 0.05 / 1, 2);
+
+        $result = [
+            "error" => False,
+            "bmi" => $body[0],
+            "bodyLevel" => $body[1],
+            "dailyCal" => $daily_calories,
+            "reqCarb" => $required_carb,
+            "reqProt" => $required_prot,
+            "reqFat" => $required_fat,
+            "reqFibr" => $required_fibr,
+        ];
+        return var_dump($result);
     }
 
     /**
@@ -85,44 +131,39 @@ class ChecksController extends Controller
      */
     public function store(Request $request)
     {
-    $request->validate([
-        'height' => 'required|numeric|min:50|max:300',
-        'weight' => 'required|numeric|min:10|max:500',
-    ], [
-        'height.required' => 'Tinggi badan harus diisi.',
-        'height.numeric' => 'Tinggi badan harus berupa angka.',
-        'height.min' => 'Tinggi badan tidak boleh kurang dari 50 cm.',
-        'height.max' => 'Tinggi badan tidak boleh melebihi 300 cm.',
-        'weight.required' => 'Berat badan harus diisi.',
-        'weight.numeric' => 'Berat badan harus berupa angka.',
-        'weight.min' => 'Berat badan tidak boleh kurang dari 10 kg.',
-        'weight.max' => 'Berat badan tidak boleh melebihi 500 kg.',
-    ]);
+        // dd($request->all());
 
-    // mengambil data pengguna yang sedang login
-    $user = Auth::user();
+        $request->validate([
+            'height' => 'required|numeric|min:50|max:300',
+            'weight' => 'required|numeric|min:10|max:500',
+            'sugar_content' => 'required|numeric',
+            'activity' => 'required|exists:activity_categories,id',
+            'test_method' => 'required|exists:test_method,id',
+        ]);
 
-    // pastikan pengguna terautentikasi
-    if (!$user) {
-        return redirect()->route('login')->with('error', 'Please login to continue.');
+        // mengambil data pengguna yang sedang login
+        $user = Auth::user();
+
+        // pastikan pengguna terautentikasi
+        if (!$user) {
+            return redirect()->route('login')->with('error', 'Please login to continue.');
+        }
+
+        // siapkan data untuk disimpan di tabel checks
+        $data = [
+            'user_id' => $user->id,
+            'height' => $request->input('weight'),
+            'weight' => $request->input('height'),
+            'sugar_content' => $request->input('sugar_content'),
+            'test_method_id' => $request->input('test_method'),
+            'activity_categories_id' => $request->input('activity'),
+        ];
+
+        ChecksModel::create($data);
+
+        // redirect ke halaman check.index
+        return redirect()->route('check.index');
     }
-
-    // siapkan data untuk disimpan di tabel checks
-    $data = [
-        'user_id' => $user->id,
-        'height' => $request->height,
-        'weight' => $request->weight,
-        'activity' => $request->activity_categories_id,
-        'sugar' => $request->sugar_content,
-        'test_method' => $request->test_method_id,
-    ];
-
-    // buat entri baru di tabel checks
-    ChecksModel::create($data);
-
-    // redirect ke halaman check.index
-    return redirect()->route('check.index');
-}
 
 
     /**
--- database/migrations/2024_09_24_093306_create_checks_tables.php
+++ database/migrations/2024_09_24_093306_create_checks_tables.php
@@ -16,9 +16,14 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->integer('weight');
             $table->integer('height');
+            $table->decimal('sugar_content', 5, 2);
+            $table->unsignedBigInteger('activity_categories_id');
+            $table->unsignedBigInteger('test_method_id');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('activity_categories_id')->references('id')->on('activity_categories')->onDelete('cascade');
+            $table->foreign('test_method_id')->references('id')->on('test_method')->onDelete('cascade');
         });
     }
 
