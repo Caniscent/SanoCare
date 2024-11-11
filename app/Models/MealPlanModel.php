a52ef80bdae3db84f9f63ac0314ca71f37f401f3
Adrella Arte <oujireflect.me@gmail.com>
2024-11-11 09:22:03 +0700

    penambahan user_id pada tabel meal_plans

--- app/Models/MealPlanModel.php
+++ app/Models/MealPlanModel.php
@@ -21,4 +21,8 @@ class MealPlanModel extends Model
     public function measurement() {
         return $this->belongsTo(MeasurementModel::class,'measurement_id');
     }
+
+    public function user() {
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
